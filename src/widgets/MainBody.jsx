39ead988ae4756eee693b1c3a9c0af5cc8cf5c13
EHoTiNKA <alexe.pyshkin@yandex.ru>
2023-04-17 13:55:07 +0300

    Search and item cards

--- src/widgets/MainBody.jsx
+++ src/widgets/MainBody.jsx
@@ -5,7 +5,6 @@ import MainCard from "../components/MainCard";
 import PosterMak from "../assets/posterMak.svg";
 import Eda1 from "../assets/eda1.svg";
 import Eda2 from "../assets/eda2.svg";
-import Brok from "../assets/brokol.jpg";
 
 const MainBody = () => {
   return (
@@ -14,30 +13,44 @@ const MainBody = () => {
         <Search />
         <p className="p1">Рестораны в Москве</p>
         <div className="cardsContent">
-          <div className="card">
-            <MainCard
-              poster={PosterMak}
-              name="Макдональдс - Газетный"
-              text1="₽ • Бургеры"
-              text2="25-35 мин"
-            />
-          </div>
-          <div className="card">
-            <MainCard
-              poster={Eda1}
-              name="DimSum & Co — ЦДМ"
-              text1="₽ • Японская • Китайская • Азиатская"
-              text2="40 - 50 мин"
-            />
-          </div>
-          <div className="card">
-            <MainCard
-              poster={Eda2}
-              name="Bar BQ Cafe — Манежная"
-              text1="₽ • Европейская"
-              text2="30 - 40 мин"
-            />
-          </div>
+          <MainCard
+            poster={PosterMak}
+            name="Макдональдс - Газетный"
+            text1="₽ • Бургеры"
+            text2="25-35 мин"
+          />
+
+          <MainCard
+            poster={Eda1}
+            name="DimSum & Co — ЦДМ"
+            text1="₽ • Японская • Китайская • Азиатская"
+            text2="40 - 50 мин"
+          />
+
+          <MainCard
+            poster={Eda2}
+            name="Bar BQ Cafe — Манежная"
+            text1="₽ • Европейская"
+            text2="30 - 40 мин"
+          />
+          <MainCard
+            poster={Eda2}
+            name="Bar BQ Cafe — Манежная"
+            text1="₽ • Европейская"
+            text2="30 - 40 мин"
+          />
+          <MainCard
+            poster={Eda2}
+            name="Bar BQ Cafe — Манежная"
+            text1="₽ • Европейская"
+            text2="30 - 40 мин"
+          />
+          <MainCard
+            poster={Eda2}
+            name="Bar BQ Cafe — Манежная"
+            text1="₽ • Европейская"
+            text2="30 - 40 мин"
+          />
         </div>
       </ContentColumn>
     </div>
